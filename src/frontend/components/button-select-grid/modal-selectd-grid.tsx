f1b626b3af137868132b19ebd177f6e30dbf4728
Belkross <belkross.pro@gmail.com>
2023-01-04 19:54:28 +0100

    fix: modal didn’t appear and add responsiveness

--- src/frontend/components/button-select-grid/modal-selectd-grid.tsx
+++ src/frontend/components/button-select-grid/modal-selectd-grid.tsx
@@ -13,6 +13,8 @@ import useValidTextInput from "../../customHooks/use-valid-text-input.js";
 import checkGridIdValidity from "./check-grid-id-validity.js";
 import { socket } from "../../config/initialize-socket-io.js";
 import { useGameState } from "../provider-game-state/provider-game-state.js";
+import useMediaQuery from "@mui/material/useMediaQuery";
+import { useTheme, Breakpoint } from "@mui/material/styles";
 
 interface ModalSelectGridProps {
 	displayState: boolean;
@@ -23,6 +25,7 @@ export default function ModalSelectGrid({ displayState, closeModal }: ModalSelec
 	const gameState = useGameState();
 	const [input, onInputChange] = useValidTextInput(gameState.selectedGrid.toString(), checkGridIdValidity);
 	const [cleanGridPreference, setCleanGridPreference] = useState(false);
+	const fullScreen = useMediaQuery(useTheme().breakpoints.down("thin" as Breakpoint));
 
 	const handleCheckboxChange = () => setCleanGridPreference(cleanGridPreference ? false : true);
 
@@ -41,14 +44,15 @@ export default function ModalSelectGrid({ displayState, closeModal }: ModalSelec
 	};
 
 	return (
-		<Dialog open={displayState} onClose={closeModal}>
+		<Dialog open={displayState} onClose={closeModal} sx={style_dialog} fullScreen={fullScreen} maxWidth={false}>
 			<DialogTitle>Changer de grille</DialogTitle>
+
 			<DialogContent>
 				<DialogContentText sx={style_dialogContentText}>{`Choisissez un numéro de grille compris entre 1 et ${NUMBER_OF_GRIDS}.`}</DialogContentText>
 				<TextField autoFocus label="numéro de grille" fullWidth value={input.value} onChange={onInputChange} onKeyDown={handleKeyDown} />
-
 				<FormControlLabel label="Nettoyer la grille" control={<Checkbox checked={cleanGridPreference} onChange={handleCheckboxChange} />} />
 			</DialogContent>
+
 			<DialogActions>
 				<Button onClick={handleSubmit} disabled={!input.validity}>
 					Valider
@@ -57,6 +61,10 @@ export default function ModalSelectGrid({ displayState, closeModal }: ModalSelec
 		</Dialog>
 	);
 }
+
+const style_dialog = {
+	margin: "auto",
+};
 
 const style_dialogContentText = {
 	marginBottom: 2,
