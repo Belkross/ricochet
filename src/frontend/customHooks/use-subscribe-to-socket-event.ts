7698f1304b21d3b390ddb200be44914cbeb46ea9
Belkross <belkross.pro@gmail.com>
2023-01-04 20:21:09 +0100

    refactor: improve type coverage

--- src/frontend/customHooks/use-subscribe-to-socket-event.ts
+++ src/frontend/customHooks/use-subscribe-to-socket-event.ts
@@ -1,14 +1,14 @@
-import { useEffect } from "react";
+import { DependencyList, useEffect } from "react";
 import { socket } from "../config/initialize-socket-io.js";
 
 export type subscribeSocketEventParams = {
 	eventName: string;
 	action: (...parameters: any[]) => void;
-	effectDependencies: any[] | undefined;
+	effectDependencies?: DependencyList;
 };
 
 export default function useSubscribeSocketEvent(params: subscribeSocketEventParams): void {
-	useEffect((): any => {
+	useEffect((): (() => void) => {
 		socket.on(params.eventName, (...serverParams: any[]) => params.action(...serverParams));
 
 		return () => socket.off(params.eventName);
