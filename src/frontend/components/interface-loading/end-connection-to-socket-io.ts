bb55d0666c22c0eb44e90e171b48231ad5507e7d
Belkross <belkross.pro@gmail.com>
2023-01-07 18:17:21 +0100

    refactor: rewrite constant value with uppercase convention

--- src/frontend/components/interface-loading/end-connection-to-socket-io.ts
+++ src/frontend/components/interface-loading/end-connection-to-socket-io.ts
@@ -1,8 +1,8 @@
 import { AppStateChanger } from "../../types.js";
+const FAKE_LOADING_DURATION = 500;
 
 export default function endConnectionToSocketIo(changeAppState: AppStateChanger): void {
 	changeAppState({ state: "loadingApp" });
 
-	const fakeLoadingDuration = 500;
-	setTimeout(() => changeAppState({ state: "logging" }), fakeLoadingDuration);
+	setTimeout(() => changeAppState({ state: "logging" }), FAKE_LOADING_DURATION);
 }
