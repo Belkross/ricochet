9c0bad57eda8e925d4840e3ec364fea1fd912d25
Belkross <belkross.pro@gmail.com>
2023-03-27 21:14:44 +0200

    feature: prevent the user from selecting a grid that don’t exist

--- src/components/grid-selection.tsx
+++ src/components/grid-selection.tsx
@@ -2,23 +2,28 @@ import { IconButton, Stack, SxProps, Typography } from "@mui/material"
 import { Dispatch, SetStateAction } from "react"
 import ArrowRight from "@mui/icons-material/ArrowForwardIos"
 import ArrowLeft from "@mui/icons-material/ArrowBackIosNew"
+import { grids } from "../assets/grids.js"
 
 type Props = {
   appState: AppState
   setAppState: Dispatch<SetStateAction<AppState>>
 }
 
+const minGridId = 1
+const maxGridId = Object.keys(grids).length
+
 export function GridSelection({ appState, setAppState }: Props) {
+  const { gridId } = appState
   const handleClickRight = () => setAppState((prevState) => ({ ...prevState, gridId: ++prevState.gridId }))
   const handleClickLeft = () => setAppState((prevState) => ({ ...prevState, gridId: --prevState.gridId }))
 
   return (
     <Stack sx={style_container}>
-      <IconButton aria-label="grille précédente" onClick={handleClickLeft}>
+      <IconButton aria-label="grille précédente" onClick={handleClickLeft} disabled={gridId <= minGridId}>
         <ArrowLeft />
       </IconButton>
-      <Typography>Grille n°{appState.gridId}</Typography>
-      <IconButton aria-label="grille suivante" onClick={handleClickRight}>
+      <Typography>Grille n°{gridId}</Typography>
+      <IconButton aria-label="grille suivante" onClick={handleClickRight} disabled={gridId >= maxGridId}>
         <ArrowRight />
       </IconButton>
     </Stack>
@@ -28,5 +33,5 @@ export function GridSelection({ appState, setAppState }: Props) {
 const style_container: SxProps = {
   flexFlow: "row nowrap",
   gap: 1,
-  alignItems: "center"
+  alignItems: "center",
 }
