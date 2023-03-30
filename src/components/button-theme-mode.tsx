fedacf51b529259f2217442e57638d44e05ba91b
Belkross <belkross.pro@gmail.com>
2023-03-30 17:33:11 +0200

    feature: add a label to dark mode button

--- src/components/button-theme-mode.tsx
+++ src/components/button-theme-mode.tsx
@@ -1,6 +1,6 @@
 import DarkModeIcon from "@mui/icons-material/DarkMode"
 import LightModeIcon from "@mui/icons-material/LightMode"
-import IconButton from "@mui/material/IconButton"
+import { Button } from "@mui/material"
 import { useThemeMode, useToggleThemeMode } from "./provider-theme-mode.js"
 
 export function ButtonThemeMode() {
@@ -8,8 +8,8 @@ export function ButtonThemeMode() {
   const toggleThemeMode = useToggleThemeMode()
 
   const icon = themeMode === "dark" ? <DarkModeIcon /> : <LightModeIcon />
-
+  
   const handleClick = () => toggleThemeMode()
 
-  return <IconButton onClick={handleClick}>{icon}</IconButton>
+  return <Button onClick={handleClick} startIcon={icon}>Thème</Button>
 }
