7140472b48ea698d207edea3ba7b4e399a069308
Belkross <belkross.pro@gmail.com>
2023-03-31 15:13:19 +0200

    refactor: remove repeated code

--- src/theme/shape.ts
+++ src/theme/shape.ts
@@ -4,12 +4,7 @@ const shape = {
   gridMaxWidth: "550px",
 
   borderRadius: 2.5,
-  borderMain: {
-    borderWidth: { xs: "2px", md: "3px" },
-    borderStyle: "solid",
-    borderColor: "background.border",
-  },
-
+  
   spacingBase: { xs: 1.5, sm: 2, md: 2.5, lg: 3 },
 }
 
