8bb044981d6f38b42ca6a32ee1ce63944952a984
Belkross <belkross.pro@gmail.com>
2023-04-04 18:04:58 +0200

    chore: remove unused feature: home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
-import { useMemo, useState } from "react"
+import { useState } from "react"
 import { GlobalFeatures } from "./components/global-features.js"
 import { InterfaceGame } from "./components/interface-game.js"
-import { InterfaceHome } from "./components/interface-home.js"
 
 const initialAppState: AppState = {
-  status: "game",
   selectedGrid: 1,
   selectedPebble: NaN,
   wordSpots: Array(25).fill(NaN),
@@ -12,19 +10,10 @@ const initialAppState: AppState = {
 
 export default function App() {
   const [appState, setAppState] = useState(initialAppState)
-  const cachedSetAppState = useMemo(() => setAppState, [])
 
-  let appInterface
-  switch (appState.status) {
-    case "home":
-      appInterface = <InterfaceHome setAppState={cachedSetAppState} />
-      break
-    case "game":
-      appInterface = <InterfaceGame appState={appState} setAppState={cachedSetAppState} />
-      break
-    default:
-      appInterface = <p>app interface status error</p>
-  }
-
-  return <GlobalFeatures>{appInterface}</GlobalFeatures>
+  return (
+    <GlobalFeatures>
+      <InterfaceGame appState={appState} setAppState={setAppState} />
+    </GlobalFeatures>
+  )
 }
--- src/types/types.d.ts
+++ src/types/types.d.ts
@@ -1,7 +1,6 @@
 type ThemeMode = "light" | "dark"
 
 type AppState = {
-  status: "home" | "game"
   selectedGrid: number
   selectedPebble: number
   wordSpots: number[]
