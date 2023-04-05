9d73011032976736a87db4acac8e60c8c9163ea1
Belkross <belkross.pro@gmail.com>
2023-04-05 17:14:27 +0200

    fix: global styles were not using the custom mui theme

--- src/components/provider-mui-theming.tsx
+++ src/components/provider-mui-theming.tsx
@@ -1,4 +1,4 @@
-import { CssBaseline, GlobalStyles, ThemeProvider } from "@mui/material"
+import { CssBaseline, GlobalStyles, Theme, ThemeProvider, useMediaQuery } from "@mui/material"
 import { ReactElement } from "react"
 import { createCustomizedMuiTheme } from "../theme/create-customized-mui-theme.js"
 import { useThemeMode } from "./provider-theme-mode.js"
@@ -9,49 +9,56 @@ type Props = {
 
 export function ProviderMuiTheming({ children }: Props) {
   const themeMode = useThemeMode()
+  const muiTheme = createCustomizedMuiTheme(themeMode)
 
   return (
-    <ThemeProvider theme={createCustomizedMuiTheme(themeMode)}>
+    <ThemeProvider theme={muiTheme}>
       <CssBaseline enableColorScheme />
-      {StaticGlobalStyles}
+      {StaticGlobalStyles(muiTheme)}
       {children}
     </ThemeProvider>
   )
 }
 
-const StaticGlobalStyles = (
-  <GlobalStyles
-    styles={{
-      body: {
-        display: "flex",
-        placeItems: "center",
-        height: "100vh",
+const StaticGlobalStyles = (theme: Theme) => {
+  const smallScreen = useMediaQuery(theme.breakpoints.down("lg"))
 
-        "&::-webkit-scrollbar, & *::-webkit-scrollbar": {
-          backgroundColor: "inherit",
-          width: "10px",
-        },
-        "&::-webkit-scrollbar-thumb, & *::-webkit-scrollbar-thumb": {
-          backgroundColor: "#6b6b6b",
-          minHeight: "30px",
-        },
-        "&::-webkit-scrollbar-thumb:focus, & *::-webkit-scrollbar-thumb:focus": {
-          backgroundColor: "#959595",
-        },
-        "&::-webkit-scrollbar-thumb:active, & *::-webkit-scrollbar-thumb:active": {
-          backgroundColor: "#959595",
-        },
-        "&::-webkit-scrollbar-thumb:hover, & *::-webkit-scrollbar-thumb:hover": {
-          backgroundColor: "#959595",
-        },
-        "&::-webkit-scrollbar-corner, & *::-webkit-scrollbar-corner": {
-          backgroundColor: "inherit",
-        },
-      },
+  return (
+    <GlobalStyles
+      styles={{
+        body: {
+          margin: 0,
+          display: "flex",
+          justifyContent: "center",
+          alignItems: smallScreen ? "start" : "center",
+          minHeight: "100vh",
 
-      "#root": {
-        width: "100%",
-      },
-    }}
-  />
-)
+          "&::-webkit-scrollbar, & *::-webkit-scrollbar": {
+            backgroundColor: "inherit",
+            width: "10px",
+          },
+          "&::-webkit-scrollbar-thumb, & *::-webkit-scrollbar-thumb": {
+            backgroundColor: "#6b6b6b",
+            minHeight: "30px",
+          },
+          "&::-webkit-scrollbar-thumb:focus, & *::-webkit-scrollbar-thumb:focus": {
+            backgroundColor: "#959595",
+          },
+          "&::-webkit-scrollbar-thumb:active, & *::-webkit-scrollbar-thumb:active": {
+            backgroundColor: "#959595",
+          },
+          "&::-webkit-scrollbar-thumb:hover, & *::-webkit-scrollbar-thumb:hover": {
+            backgroundColor: "#959595",
+          },
+          "&::-webkit-scrollbar-corner, & *::-webkit-scrollbar-corner": {
+            backgroundColor: "inherit",
+          },
+        },
+
+        "#root": {
+          width: "100%",
+        },
+      }}
+    />
+  )
+}
