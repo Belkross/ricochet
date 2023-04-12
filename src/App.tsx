6c25a84410ec61484773aa824cc9fb37a1e5823d
Belkross <belkross.pro@gmail.com>
2023-04-12 13:30:37 +0200

    refactor: create a function that initialize the app state

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,10 @@
 import { useState } from "react"
 import { GlobalFeatures } from "./components/global-features.js"
 import { InterfaceGame } from "./components/interface-game.js"
-import { getInitialGridId } from "./functions/get-initial-grid-id.js"
-import { localStorageKeys } from "./config/local-storage-keys.js"
-import { minGridId } from "./assets/grids.js"
-import { getInitialWordSpots } from "./functions/get-initial-word-spots.js"
-
-const initialAppState: AppState = {
-  selectedGrid: getInitialGridId(localStorageKeys.gridId, minGridId),
-  selectedPebble: NaN,
-  wordSpots: getInitialWordSpots(localStorageKeys.wordSpots, Array(25).fill(NaN)),
-}
+import { initializeAppState } from "./functions/initialize-app-state.js"
 
 export default function App() {
-  const [appState, setAppState] = useState(initialAppState)
+  const [appState, setAppState] = useState(initializeAppState())
 
   return (
     <GlobalFeatures>
---
+++ src/functions/initialize-app-state.ts
@@ -0,0 +1,12 @@
+import { minGridId } from "../assets/grids.js"
+import { localStorageKeys } from "../config/local-storage-keys.js"
+import { getInitialGridId } from "./get-initial-grid-id.js"
+import { getInitialWordSpots } from "./get-initial-word-spots.js"
+
+export function initializeAppState(): AppState {
+  return {
+    selectedGrid: getInitialGridId(localStorageKeys.gridId, minGridId),
+    selectedPebble: NaN,
+    wordSpots: getInitialWordSpots(localStorageKeys.wordSpots, Array(25).fill(NaN)),
+  }
+}
