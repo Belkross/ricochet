011e2078b4f55f4e0ae1ebf983ab8a8b9ac49144
Belkross <belkross.pro@gmail.com>
2023-04-22 00:43:31 +0200

    perf: memoize context’s provided value

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useReducer } from "react"
+import { useMemo, useReducer } from "react"
 import { GlobalFeatures } from "./components/global-features.js"
 import { InterfaceGame } from "./components/interface-game.js"
 import { initializeAppState } from "./functions/initialize-app-state.js"
@@ -8,9 +8,11 @@ import { AppStateDispatchContext } from "./contexts/context-app-state.js"
 export default function App() {
   const [appState, dispatch] = useReducer(reducerAppState, initializeAppState())
 
+  const dispatchMemoized = useMemo(() => dispatch, [])
+
   return (
     <GlobalFeatures>
-      <AppStateDispatchContext.Provider value={dispatch}>
+      <AppStateDispatchContext.Provider value={dispatchMemoized}>
         <InterfaceGame appState={appState} />
       </AppStateDispatchContext.Provider>
     </GlobalFeatures>
