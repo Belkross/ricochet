b0d327e12fb4af216179a800e0caac679e16d07b
Belkross <belkross.pro@gmail.com>
2023-05-04 05:18:24 +0200

    feat: provide an aria-label to pebbles because their accessible name don’t describe accurately the element

--- src/components/pebble-list.tsx
+++ src/components/pebble-list.tsx
@@ -11,7 +11,12 @@ export function PebbleList() {
 
   const list_pebbles = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11].map((pebbleId: number) => {
     return (
-      <Button key={pebbleId} sx={style_button(pebbleId, appState)} onClick={() => handleClick(pebbleId)}>
+      <Button
+        key={pebbleId}
+        sx={style_button(pebbleId, appState)}
+        onClick={() => handleClick(pebbleId)}
+        aria-label={`galet ${pebbleId}`}
+      >
         <Typography>{pebbleId}</Typography>
         <RemainingPebbleVisual pebbleId={pebbleId} appState={appState} />
       </Button>
