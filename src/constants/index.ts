cd81a8bd4cb1e8b705ae02e4f700a31684b951fe
Belkross <belkross.pro@gmail.com>
2023-05-04 22:56:10 +0200

    feat: add some app constants

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -2,3 +2,9 @@ import { grids } from "../assets/grids"
 
 export const MIN_GRID_ID = 1
 export const MAX_GRID_ID = Object.keys(grids).length
+
+export const MIN_PEBBLE_ID = 1
+export const MAX_PEBBLE_ID = 11
+
+export const MIN_WORD_ID = 1
+export const MAX_WORD_ID = 25
