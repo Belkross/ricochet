fcda1e41f5439ea334c4e4e1aca7c352d54d6b8a
belkross <belkross.pro@gmail.com>
2024-04-16 22:13:15 +0200

    test: unit testing clicking on pebble

--- test/unit/reducer-app-state.test.ts
+++ test/unit/reducer-app-state.test.ts
@@ -1,9 +1,9 @@
-import { MAX_GRID_ID, MIN_GRID_ID } from "#domain"
+import { MAX_GRID_ID, MIN_GRID_ID, MIN_PEBBLE_ID } from "#domain"
 import { initializeAppState } from "../../src/store/initialize-app-state"
 import { reducerAppState } from "../../src/store/reducer-app-state"
 import { ActionType } from "../../src/type/app-state.type"
 
-describe("decrementing the grid", () => {
+describe(ActionType.decrement_grid, () => {
   test("when the current grid is the first one it should keep it", () => {
     const state = initializeAppState()
     state.selectedGrid = MIN_GRID_ID
@@ -23,7 +23,7 @@ describe("decrementing the grid", () => {
   })
 })
 
-describe("incrementing the grid", () => {
+describe(ActionType.increment_grid, () => {
   test("when the current grid is the last one should keep it" + "should display the ad", () => {
     const state = initializeAppState()
     state.selectedGrid = MAX_GRID_ID
@@ -42,5 +42,37 @@ describe("incrementing the grid", () => {
     const updatedState = reducerAppState(state, { type: ActionType.increment_grid })
 
     expect(updatedState.selectedGrid).toBe(MIN_GRID_ID + 1)
+  })
+})
+
+describe(ActionType.click_pebble, () => {
+  test("getting a pebble", () => {
+    const state = initializeAppState()
+    state.selectedPebble = NaN
+
+    const updatedState = reducerAppState(state, { type: ActionType.click_pebble, payload: MIN_PEBBLE_ID })
+
+    expect(updatedState.selectedPebble).toBe(MIN_PEBBLE_ID)
+  })
+
+  test("clicking on a while already having the same on hand", () => {
+    const state = initializeAppState()
+    const pebble = MIN_PEBBLE_ID
+    state.selectedPebble = pebble
+
+    const updatedState = reducerAppState(state, { type: ActionType.click_pebble, payload: pebble })
+
+    expect(updatedState.selectedPebble).toBe(NaN)
+  })
+
+  test("clicking on a pebble when no pebble left", () => {
+    const state = initializeAppState()
+    const pebble = MIN_PEBBLE_ID
+    state.wordSpots[0] = pebble
+    state.wordSpots[1] = pebble
+
+    const updatedState = reducerAppState(state, { type: ActionType.click_pebble, payload: pebble })
+
+    expect(updatedState.selectedPebble).toBe(NaN)
   })
 })
